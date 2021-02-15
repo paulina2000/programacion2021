d420acbc066cd9bd7866d8e98b3b9cde3100f121
paulina2000 <paulina.orozco2000@gmail.com>
2021-02-14 20:20:50 -0500

    subir clase 3

---
+++ clases programacion/clase2.py
@@ -0,0 +1,26 @@
+# estos son booleans que son solo variables
+# verdadero o falso
+pruebaV = True
+pruebaF = False
+print (pruebaF)
+print (pruebaV)
+# estos son datos generales sobre mi
+edad = 20
+estatura = 1.75
+peso = 61
+NOMBRE = "Paulina Orozco Ruiz"
+# aqui preguntamos si soy mayor de edad
+print ("#"*15,"Mayor Edad", "#"*15)
+isMayorEdad = edad >= 18
+print (isMayorEdad)
+print ("#"*15,"bajo la estatura promedio", "#"*15)
+isMayorEstatura = estatura < 1.70
+print(isMayorEstatura)
+print ("#"*15,"peso diferente 84", "#"*15)
+isPesoDiferente = peso != 84
+print (isPesoDiferente) 
+# vamos a ver si un apellido esta en el nombre completo
+apellido = "mejia"
+isApellido = apellido in NOMBRE
+print ("#"*15,"esta apellido en el nombre", "#"*15)
+print (isApellido)
